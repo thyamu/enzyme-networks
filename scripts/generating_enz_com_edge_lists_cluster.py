02a993388c5ac91ef19285cfc99942d16022ba30
thyamu <hkim8@alumni.nd.edu>
2021-07-19 14:48:25 -0700

    Created a script for cluster.

--- scripts/generating_enz_com_edge_lists_cluster.py
+++ scripts/generating_enz_com_edge_lists_cluster.py
@@ -1,7 +1,6 @@
-import os
+import sys
 import json
 import networkx as nx
-import itertools
 import time
 
 ###
@@ -10,10 +9,12 @@ def load_file_of_kegg_enzyme_substrates_products():
         a = json.load(f)
     return a
 
+
 def load_list_enzyme_for_domain(path_enz_domain):
     with open(path_enz_domain, "r") as f:
         e = json.load(f)
     return e
+
 
 def generate_unipartite_enz_net_for_domain(domain):
 
@@ -53,18 +54,110 @@ def generate_unipartite_enz_net_for_domain(domain):
         nx.write_gpickle(G, dir_net + "/enz_net_%s_%s.gpickle"%(domain, taxa))
 
 
+def assign_taxa_index_to_domain_data(dict_data, taxa_index):
+    list_taxa = sorted(dict_data.keys())
+    selected_taxa = list_taxa[taxa_index]
+    return selected_taxa
+
+def generate_unipartite_enz_net_for_taxa(domain, taxa_index):
+
+    dir_net = "../data/Network/EnzNet/" + domain #dirtectory for output
+
+    path_enz_domain = "../data/ProcessedJGI/" + domain + "/No-Glycan/" + domain + "_ec_clean_list_no_glycan.json"
+
+    dict_taxa_enz_in_jgi = load_list_enzyme_for_domain(path_enz_domain)
+
+    taxa = assign_taxa_index_to_domain_data(dict_taxa_enz_in_jgi, taxa_index)
+
+    dict_enz_com_in_kegg = load_file_of_kegg_enzyme_substrates_products()
+
+    B = nx.DiGraph()
+    list_comp = list()
+    for enz in dict_taxa_enz_in_jgi[taxa]:
+
+        if enz not in dict_enz_com_in_kegg:
+            continue
+
+        for subs in dict_enz_com_in_kegg[enz]["subs"]:
+            B.add_edge(subs,enz)
+            list_comp.append(subs)
+
+        for prod in dict_enz_com_in_kegg[enz]["prod"]:
+            B.add_edge(enz, prod)
+            list_comp.append(prod)
+
+
+    G = nx.DiGraph()
+    for n in list_comp:
+        for e1 in B.predecessors(n):
+            for e2 in B.successors(n):
+                G.add_edge(e1,e2)
+
+    nx.write_gpickle(G, dir_net + "/enz_net_%s_%s.gpickle"%(domain, taxa))
+
+
+def assign_list_taxa_index_to_domain_data(dict_data, taxa_index, list_length):
+    list_taxa = sorted(dict_data.keys())
+    selected_taxa_list = list_taxa[taxa_index: taxa_index + list_length]
+    return selected_taxa_list
+
+
+def generate_unipartite_enz_net_for_group_of_taxa(domain, taxa_index, list_length = 100):
+
+    dir_net = "../data/Network/EnzNet/" + domain #dirtectory for output
+
+    path_enz_domain = "../data/ProcessedJGI/" + domain + "/No-Glycan/" + domain + "_ec_clean_list_no_glycan.json"
+
+    dict_taxa_enz_in_jgi = load_list_enzyme_for_domain(path_enz_domain)
+
+    list_taxa = assign_list_taxa_index_to_domain_data(dict_taxa_enz_in_jgi, taxa_index, list_length)
+
+    dict_enz_com_in_kegg = load_file_of_kegg_enzyme_substrates_products()
+
+    for taxa in list_taxa:
+        B = nx.DiGraph()
+        list_comp = list()
+        for enz in dict_taxa_enz_in_jgi[taxa]:
+
+            if enz not in dict_enz_com_in_kegg:
+                continue
+
+            for subs in dict_enz_com_in_kegg[enz]["subs"]:
+                B.add_edge(subs,enz)
+                list_comp.append(subs)
+
+            for prod in dict_enz_com_in_kegg[enz]["prod"]:
+                B.add_edge(enz, prod)
+                list_comp.append(prod)
+
+
+        G = nx.DiGraph()
+        for n in list_comp:
+            for e1 in B.predecessors(n):
+                for e2 in B.successors(n):
+                    G.add_edge(e1,e2)
+
+        nx.write_gpickle(G, dir_net + "/enz_net_%s_%s.gpickle"%(domain, taxa))
 
 
 ###########################################################################################
-list_domain = ["Archaea", "Bacteria", "Eukaryota", "Metagenome", "Biosphere", "LUCA"]
 
-domain = "LUCA"
+# list_domain = ["Archaea", "Bacteria", "Eukaryota", "Metagenome", "Biosphere", "LUCA"]
 
-bt = time.time()
-a = generate_unipartite_enz_net_for_domain(domain)
+domain = "Metagenome"
 
-et = time.time()
+array_index = int(sys.argv[1])
+array_gap = 100
 
-t = et - bt
+starting_taxa_index = array_index * array_gap
+array_size = array_gap
+if starting_taxa_index == 11900:
+    array_size = 55
+
+print(starting_taxa_index, starting_taxa_index + array_size)
+
+# generate_unipartite_enz_net_for_group_of_taxa(domain, starting_taxa_index, array_size)
+
+
 
 
