210a2298781f3af00b882427e720b51e660c2800
thyamu <hkim8@alumni.nd.edu>
2021-07-27 17:05:36 +0200

    Added a script for the cluster to measure the distributions of node centrality over EC classes in metagenome.

--- scripts/measure_betweenness_cluster.py
+++ scripts/measure_betweenness_cluster.py
@@ -37,14 +37,46 @@ def betweenness_EC_net(G):
     return bet_eclass
 
 
+def load_list_enzyme_for_domain(path_enz_domain):
+    with open(path_enz_domain, "r") as f:
+        e = json.load(f)
+    return e
+
+
 def assign_list_taxa_index_to_domain_data(dict_data, taxa_index, list_length):
     list_taxa = sorted(dict_data.keys())
     selected_taxa_list = list_taxa[taxa_index: taxa_index + list_length]
     return selected_taxa_list
 
 
-def betweenness_EC_domain(domain):
-    path_nets = "../data/Network/EnzNet/%s/*.gpickle"%domain
+# def betweenness_EC_domain(domain):
+#     path_nets = "../data/Network/EnzNet/%s/*.gpickle"%domain
+#
+#     list_eclass = ["e1", "e2", "e3", "e4", "e5", "e6"]
+#
+#     bet_eclass_domain = dict()
+#     for ec in range(1, len(list_eclass) + 1):
+#         bet_eclass_domain[ec] = list()
+#
+#     for f in glob.glob(path_nets):
+#         G = load_ezn_net(f)
+#         distribution_betweenness = betweenness_EC_net(G)
+#         for ec in range(1, len(list_eclass) + 1):
+#             bet_eclass_domain[ec] = bet_eclass_domain[ec] + distribution_betweenness[ec]
+#
+#     return bet_eclass_domain
+
+
+def betweenness_EC_group_of_taxa(domain, taxa_index, list_length = 100):
+
+
+    path_enz_domain = "../data/ProcessedJGI/" + domain + "/No-Glycan/" + domain + "_ec_clean_list_no_glycan.json"
+
+    dict_taxa_enz_in_jgi = load_list_enzyme_for_domain(path_enz_domain)
+
+    list_taxa = assign_list_taxa_index_to_domain_data(dict_taxa_enz_in_jgi, taxa_index, list_length)
+
+    # path_nets = "../data/Network/EnzNet/%s/*.gpickle"%domain
 
     list_eclass = ["e1", "e2", "e3", "e4", "e5", "e6"]
 
@@ -52,25 +84,8 @@ def betweenness_EC_domain(domain):
     for ec in range(1, len(list_eclass) + 1):
         bet_eclass_domain[ec] = list()
 
-    for f in glob.glob(path_nets):
-        G = load_ezn_net(f)
-        distribution_betweenness = betweenness_EC_net(G)
-        for ec in range(1, len(list_eclass) + 1):
-            bet_eclass_domain[ec] = bet_eclass_domain[ec] + distribution_betweenness[ec]
-
-    return bet_eclass_domain
-
-
-def betweenness_EC_group_of_taxa(domain, taxa_index, list_length):
-    path_nets = "../data/Network/EnzNet/%s/*.gpickle"%domain
-
-    list_eclass = ["e1", "e2", "e3", "e4", "e5", "e6"]
-
-    bet_eclass_domain = dict()
-    for ec in range(1, len(list_eclass) + 1):
-        bet_eclass_domain[ec] = list()
-
-    for f in glob.glob(path_nets):
+    for taxa in list_taxa:
+        f = "../data/Network/EnzNet/%s/enz_net_%s_%s.gpickle"%(domain, domain, taxa)
         G = load_ezn_net(f)
         distribution_betweenness = betweenness_EC_net(G)
         for ec in range(1, len(list_eclass) + 1):
@@ -96,7 +111,8 @@ def betweenness_EC_group_of_taxa(domain, taxa_index, list_length):
 #
 #     return bet_eclass_domain
 
-def write_results(domain, result):
+
+def write_results_group_of_taxa(domain, result, index):
 
     dir_results = "../results"
     if not os.path.exists(dir_results):
@@ -110,32 +126,56 @@ def write_results(domain, result):
     if not os.path.exists(dir_betweenness):
         os.mkdir(dir_betweenness)
 
-    path_file  = os.path.join(dir_betweenness, "betweenness_distribution_over_EC_%s.json"%domain)
+    path_file  = os.path.join(dir_betweenness, "betweenness_distribution_over_EC_%s_%s.json"%(domain, index))
     with open(path_file, "w") as file:
         json.dump(result, file)
 
 
-def test():
-    print("this is test")
-    bt = time.time()
-    domain = "LUCA"
-    betweenness_dist = betweenness_EC_domain(domain)
-    write_results(domain, betweenness_dist)
-    et = time.time()
-    t = et - bt
-    print(t)
+# def write_results(domain, result):
+#
+#     dir_results = "../results"
+#     if not os.path.exists(dir_results):
+#         os.mkdir(dir_results)
+#
+#     dir_topology = os.path.join(dir_results, "topology")
+#     if not os.path.exists(dir_topology):
+#         os.mkdir(dir_topology)
+#
+#     dir_betweenness = os.path.join(dir_topology, "betweenness")
+#     if not os.path.exists(dir_betweenness):
+#         os.mkdir(dir_betweenness)
+#
+#     path_file  = os.path.join(dir_betweenness, "betweenness_distribution_over_EC_%s.json"%domain)
+#     with open(path_file, "w") as file:
+#         json.dump(result, file)
 
 
-def main():
+# def test():
+#     print("this is test")
+#     bt = time.time()
+#     domain = "LUCA"
+#     betweenness_dist = betweenness_EC_group_of_taxa(domain)
+#     write_results_group_of_taxa(domain, betweenness_dist)
+#     et = time.time()
+#     t = et - bt
+#     print(t)
+
+
+def main(arg):
     # name_eclass = ["oxidoreductase", "transferase", "hydrolase", "lysase", "isomerase", "ligase"]
 
-    # list_domain = ["Archaea", "Bacteria", "Eukaryota", "Metagenome", "Biosphere", "LUCA"]
-    #list_domain = ["Archaea", "Eukaryota", "Biosphere", "LUCA"]
-    #for domain in list_domain:
-    domain = "Bacteria"
+    domain = "Metagenome"
     print(domain)
-    betweenness_dist = betweenness_EC_domain(domain)
-    write_results(domain, betweenness_dist)
+    array_index = int(sys.argv[1])
+    array_gap = 100
+
+    starting_taxa_index = array_index * array_gap
+    array_size = 1
+    if starting_taxa_index == 11900:
+        array_size = 55
+
+    betweenness_dist = betweenness_EC_group_of_taxa(domain, starting_taxa_index, array_size)
+    write_results_group_of_taxa(domain, betweenness_dist, array_index)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
