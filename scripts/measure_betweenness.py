578ebe4f7671f4346671074e6def01d6940f2ce0
thyamu <hkim8@alumni.nd.edu>
2021-08-02 18:40:58 +0200

    Added methods to compute the between centrality for all individual nodes in every network for a given domain.

--- scripts/measure_betweenness.py
+++ scripts/measure_betweenness.py
@@ -18,6 +18,20 @@ def load_ezn_net(path_net):
     return G
 
 
+def betweenness_enz_domain(domain):
+    """
+    ### To compute betweenness of every enzyme in each network
+    :param domain:
+    :return: list of dictionary of betweenness for a given domain
+    """
+    path_nets = "../data/Network/EnzNet/%s/*.gpickle" % domain
+    list_dict_betweenness = list()
+    for f in glob.glob(path_nets):
+        G = load_ezn_net(f)
+        list_dict_betweenness.append(nx.betweenness_centrality(G))
+    return list_dict_betweenness
+
+
 def betweenness_EC_net(G):
     list_eclass = ["e1", "e2", "e3", "e4", "e5", "e6"]
 
@@ -55,7 +69,7 @@ def betweenness_EC_domain(domain):
 
     return bet_eclass_domain
 
-def write_results(domain, result):
+def write_results(file_name, result):
 
     dir_results = "../results"
     if not os.path.exists(dir_results):
@@ -69,32 +83,40 @@ def write_results(domain, result):
     if not os.path.exists(dir_betweenness):
         os.mkdir(dir_betweenness)
 
-    path_file  = os.path.join(dir_betweenness, "betweenness_distribution_over_EC_%s.json"%domain)
+    #path_file  = os.path.join(dir_betweenness, "betweenness_distribution_over_EC_%s.json"%domain)
+    path_file = os.path.join(dir_betweenness, file_name)
     with open(path_file, "w") as file:
         json.dump(result, file)
 
 
-def test():
-    print("this is test")
-    bt = time.time()
-    domain = "LUCA"
-    betweenness_dist = betweenness_EC_domain(domain)
-    write_results(domain, betweenness_dist)
-    et = time.time()
-    t = et - bt
-    print(t)
+# def test():
+#     print("this is test")
+#     bt = time.time()
+#     domain = "LUCA"
+#     betweenness_dist = betweenness_EC_domain(domain)
+#     write_results(domain, betweenness_dist)
+#     et = time.time()
+#     t = et - bt
+#     print(t)
 
 
-def main():
+def betweenness_distribution_over_ec_class(list_domain):
     # name_eclass = ["oxidoreductase", "transferase", "hydrolase", "lysase", "isomerase", "ligase"]
+    for domain in list_domain:
+        print(domain)
+        betweenness_dist = betweenness_EC_domain(domain)
+        file_name = "betweenness_distribution_over_EC_%s.json"%domain
+        write_results(file_name, betweenness_dist)
 
-    # list_domain = ["Archaea", "Bacteria", "Eukaryota", "Metagenome", "Biosphere", "LUCA"]
-    #list_domain = ["Archaea", "Eukaryota", "Biosphere", "LUCA"]
-    #for domain in list_domain:
-    domain = "Bacteria"
-    print(domain)
-    betweenness_dist = betweenness_EC_domain(domain)
-    write_results(domain, betweenness_dist)
+def betweenness_for_every_enzyme(list_domain):
+    for domain in list_domain:
+        print(domain)
+        betweenness_data = betweenness_enz_domain(domain)
+        file_name = "betweenness_list_dict_enz_%s.json"%domain
+        write_results(file_name, betweenness_data)
 
 if __name__ == "__main__":
-    main()
+    # list_domain = ["Archaea", "Bacteria", "Eukaryota", "Metagenome", "Biosphere", "LUCA"]
+    list_domain = ["Bacteria"]
+    #betweenness_distribution_over_ec_class(list_domain)
+    betweenness_for_every_enzyme(list_domain)
