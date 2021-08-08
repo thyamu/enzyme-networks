26c90822d6f6501b9fcd41ae647010111d44dfcc
thyamu <hkim8@alumni.nd.edu>
2021-08-08 23:00:03 +0200

    Edited Agave batch file for generating gpickle networks for Bacteria in Agave.

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks=1                      # number of tasks (default: 1)
 #SBATCH --cpus-per-task=1               # number of CPUs per task
 #SBATCH --partition=parallel            # partition to run in (serial or parallel)
-#SBATCH --job-name=enz                  # job name
-#SBATCH --output=enz-%N-%j.out          # output file name
-#SBATCH --error=enz-%N-%j.err           # error file name
+#SBATCH --job-name=net                  # job name
+#SBATCH --output=log/enz-%N-%j.out      # output file name
+#SBATCH --error=log/enz-%N-%j.err       # error file name
 #SBATCH --time=48:00:00                 # runtime requested
 #SBATCH --mail-user=hkim78@asu.edu      # notification email
 #SBATCH --mail-type=END,FAIL            # notification type
@@ -18,4 +18,4 @@ module load python/3.7.1
 number=$((${SLURM_ARRAY_TASK_ID}-1))
 
 # run the application:
-python3 measure_betweenness_cluster.py $number
+python3 generating_enz_com_edge_lists_cluster.py  $number
