2ccfee748ab21a029601b925d02396b295d9110e
thyamu <hkim8@alumni.nd.edu>
2021-08-10 01:24:12 +0200

    Added functions to merge results of topological analysis from cluster.

--- scripts/merging_results.py
+++ scripts/merging_results.py
@@ -1,25 +1,56 @@
 """
-To combine the results of betweenness measures for metagenome
+To combine the results of betweenness measures for metagenome from cluster
 
 """
 
 import glob
 import json
 
-list_eclass = ["1", "2", "3", "4", "5", "6"]
 
-bet_eclass = dict()
-for ec in list_eclass:
-    bet_eclass[ec] = list()
 
-path = "../results/topology/betweenness/metagenome_cluster/*.json"
-for path_file in glob.glob(path):
-    print(path_file)
-    with open(path_file, 'r') as f:
-        a = json.load(f)
-        for ec in list_eclass:
-            bet_eclass[ec] = bet_eclass[ec] + a[ec]
-# results/topology/betweenness/betweenness_distribution_over_EC_Archaea.json
-path_merged = "../results/topology/betweenness/betweenness_distribution_over_EC_metagenome.json"
-with open(path_merged, 'w') as f:
-    json.dump(bet_eclass, f)
+def merging_distribution_betweenness(domain):
+    list_eclass = ["1", "2", "3", "4", "5", "6"]
+
+    bet_eclass = dict()
+    for ec in list_eclass:
+        bet_eclass[ec] = list()
+
+    path = "../results/topology/betweenness/%s_cluster/*.json"%domain
+    for path_file in glob.glob(path):
+        print(path_file)
+        with open(path_file, 'r') as f:
+            a = json.load(f)
+            for ec in list_eclass:
+                bet_eclass[ec] = bet_eclass[ec] + a[ec]
+    # results/topology/betweenness/betweenness_distribution_over_EC_Archaea.json
+    path_merged = "../results/topology/betweenness/betweenness_distribution_over_EC_%s.json"%domain
+    with open(path_merged, 'w') as f:
+        json.dump(bet_eclass, f)
+
+
+def merging_list_dict_betweenness(domain):
+    result = list()
+
+    path = "../results/topology/betweenness/list_dict_%s_cluster/*.json"%domain
+    for path_file in glob.glob(path):
+        print(path_file)
+        with open(path_file, 'r') as f:
+            a = json.load(f)
+        result = result + a
+
+    path_merged = "../results/topology/betweenness/betweenness_list_dict_enz_%s.json" % domain
+    with open(path_merged, 'w') as f:
+        json.dump(result, f)
+
+
+
+def main():
+
+    merging_distribution_betweenness("Metagenome")
+
+    for domain in ["Bacteria", "Metagenome"]:
+        merging_list_dict_betweenness(domain)
+
+
+if __name__ == "__main__":
+    main()
